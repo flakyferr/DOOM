afa0d1c79c6ab7cdc0b871611b589b2927ace812
flakyferr <flakyferr@gmail.com>
2020-03-31 20:34:56 +0200

    Fixed sndserver errors.

--- sndserv/soundsrv.c
+++ sndserv/soundsrv.c
@@ -42,6 +42,7 @@ static const char rcsid[] = "$Id: soundsrv.c,v 1.3 1997/01/29 22:40:44 b1 Exp $"
 
 #include <math.h>
 #include <sys/types.h>
+#include <string.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
